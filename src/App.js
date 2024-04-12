19d9ea60d6819ce450238d2763a4f9a8fe8bc43d
eldanaomushova <eldana956@gmail.com>
2024-04-12 19:29:10 +0600

    Create file

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import Main from "./components/Main";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+function App() {
+  return (
+    <div className="App">
+    <Router >
+      <Routes >
+        <Route path="/" element ={<Main/>}/>
+      </Routes>
+    </Router>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Navbar.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Navbar = () => {
+    return (
+        <div className='navbar-cont'>
+        Navbar
+        
+        </div>
+    )
+}
+
+export default Navbar

d6e877b69aca401513f8b177923cda9f374caa0b
eldanaomushova <eldana956@gmail.com>
2024-04-12 20:37:23 +0600

    Header

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import Main from "./components/Main";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
@@ -7,6 +8,7 @@ function App() {
     <Router >
       <Routes >
         <Route path="/" element ={<Main/>}/>
+        <Route path="/navbar" element ={<Navbar/>}/>
       </Routes>
     </Router>
     </div>
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,9 +1,21 @@
-import React from 'react'
+import React from 'react';
+import logo from '../assets/logo.jpg';
+import { SlPhone, SlMagnifier, SlHeart } from "react-icons/sl";
+import '../styles/navbar.scss';
+
 
 const Navbar = () => {
     return (
         <div className='navbar-cont'>
-        Navbar
+            <nav>
+                <ol>
+                    <a href='#mainpage'><img src={logo} alt='logo'></img></a>
+                    <a href='#mainpage'><SlPhone id='icon'/><p>0(705) 75-75-28</p></a>
+                    <input className="search" placeholder='Найти товары' /><SlMagnifier id="icon" />
+                    <SlHeart id='heart'/>
+                    
+                </ol>
+            </nav>
         
         </div>
     )

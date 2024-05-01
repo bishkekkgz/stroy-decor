19e7f8fc3cce67f461673cc47cd1cd23d21f25a8
eldanaomushova <eldana956@gmail.com>
2024-05-02 00:22:31 +0600

    Card adding window popup

---
+++ src/components/Popup.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import '../styles/popup.scss';
+const PopUp = () => {
+    return (
+        <div className="popup">
+            Товар добавлен в корзину покупок!
+        </div>
+    );
+};
+
+export default PopUp;
--- src/components/primedec/PrimeDecorPlintusNat.js
+++ src/components/primedec/PrimeDecorPlintusNat.js
@@ -1,39 +1,37 @@
 import React, { useState, useEffect } from 'react';
 import PlintusNat from '../../catalog/prdec-plintus-nat';
 import '../../styles/prdec.scss';
+import PopUp from '../Popup'; // Import the PopUp component
+
 const PrimeDecorPlintusNat = () => {
     const [counts, setCounts] = useState({});
     const [basketProducts, setBasketProducts] = useState([]);
+    const [showPopUp, setShowPopUp] = useState(false);
     
-    const increment = (id) => {
-        setCounts(prevCounts => ({
-            ...prevCounts,
-            [id]: (prevCounts[id] || 0) + 1 
-        }));
-    };
-    const decrement = (id) => {
-        if (counts[id] && counts[id] > 0) {
-            setCounts(prevCounts => ({
-                ...prevCounts,
-                [id]: prevCounts[id] - 1 
-            }));
-        }
-    };
     useEffect(() => {
         const basketProductsFromStorage = JSON.parse(localStorage.getItem('basketProducts')) || [];
         setBasketProducts(basketProductsFromStorage);
     }, []);
+
     useEffect(() => {
         localStorage.setItem('basketProducts', JSON.stringify(basketProducts));
     }, [basketProducts]);
-    const toggleBasket = (id) =>{
-        if(basketProducts.includes(id)){
-            setBasketProducts(basketProducts.filter(productId =>productId !== id ));
-        }else{
+
+    const toggleBasket = (id) => {
+        if (basketProducts.includes(id)) {
+            setBasketProducts(basketProducts.filter(productId => productId !== id ));
+        } else {
             setBasketProducts([...basketProducts, id]);
         }
-    }
+        setShowPopUp(true);
+    };
 
+    useEffect(() => {
+        const timer = setTimeout(() => {
+            setShowPopUp(false);
+        }, 2000);
+        return () => clearTimeout(timer);
+    }, [showPopUp]);
 
     return (
         <div className='primedec-container'>
@@ -48,13 +46,15 @@ const PrimeDecorPlintusNat = () => {
                                     <p className='price'>{record.price}</p>
                                     <p id="som">c</p>
                                 </div>
-                                    <button className="basket-btn" onClick={() => toggleBasket(record.id)}>В корзину</button>
+                                <button className="basket-btn" onClick={() => toggleBasket(record.id)}>В корзину</button>
                             </div>
                         ) : null}
                     </div>
                 ))}
             </div>
+            {showPopUp && <PopUp />} 
         </div>
     );
 };
+
 export default PrimeDecorPlintusNat;

2a21fc2d543767f5b1e45e0f497d3de072ef2a30
eldanaomushova <eldana956@gmail.com>
2024-06-16 17:40:20 +0600

    footer fixed

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,22 +2,23 @@ import React from 'react';
 import logo from '../assets/logo.jpg';
 import { Link } from 'react-router-dom';
 import '../styles/footer.scss';
+import { SlPhone } from "react-icons/sl";
+import { FaWhatsapp } from "react-icons/fa";
+
 const Footer = () => {
     return (
-        <div className='footer-container'>
+        <div className='footer'>
+        <line />
+            <div className='footer-container'>
             <div className='logo-container'>
                 <Link to="/"><img src={logo} alt='logo' id='logo'/></Link>
             </div>
-            <div className='navOneLineLinks'>
-                <Link to="/plintusnatyazhnye" className="links"> Плинтуса для натяжных потолков</Link>
-                <Link to="/plintus" className="links">Потолочные плинтуса</Link>
-                <Link to="/molding" className="links">Молдинги</Link>
-                <Link to="" className="links">Потолочные плитки</Link>
-                <Link to="" className="links">Розетки потолочные</Link>
+            <div className='years-container'>
+                <p className='years-text'>&#169; 2023-2024</p>
             </div>
-            <line />
-            <p className='years-text'>&#169; 2023-2024</p>
         </div>
+        </div>
+        
     )
 }
 
